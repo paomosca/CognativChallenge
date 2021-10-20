accf2f634623e4569387530bdd44732c83655599
Laura-SM <laura.sabater.musach@gmail.com>
2021-10-20 11:42:23 +0200

    Add Details component to navigator

--- App.js
+++ App.js
@@ -2,18 +2,23 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/home/Home';
+import Details from './src/screens/recipes/Details';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
-            name="Home"
-            component={Home}
-        
-          />
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
